7d7c6d38bbda6ee147e607acdf42a7cf0148d214
Adam Burmister <adam.burmister@gmail.com>
2012-12-15 13:25:13 +0000

    Footer bio

---
+++ source/javascripts/flog.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var $window = $(window);
+  // //  data-bg-parallax-speed="3"
+  // $('[data-bg-parallax-speed]').each(function(){
+  //   var $el = $(this); // assigning the object
+  //   $window.scroll(function() {
+  //     var yPos = -($window.scrollTop() / $el.data('bg-parallax-speed'));
+  //     $el.css({ backgroundPosition: '50% '+ yPos + 'px' });
+  //   });
+  // });
+});
