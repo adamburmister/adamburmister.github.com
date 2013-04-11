c4592bd3b9be1bd5fdeb5c4a09d1bd0553d90fd3
Adam Burmister <adam.burmister@gmail.com>
2013-04-11 16:38:32 +0100

    Release

---
+++ stylesheets/lib/fonts/burmister-icons/lte-ie7.js
@@ -0,0 +1,2 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+window.onload=function(){function e(e,t){var n=e.innerHTML;e.innerHTML="<span style=\"font-family: 'burmister-icons'\">"+t+"</span>"+n}var t={"icon-burmistercom-logo":"&#x62;","icon-twitter":"&#x74;","icon-linkedin":"&#x69;","icon-github":"&#x67;"},n=document.getElementsByTagName("*"),r,i,s,o,u;for(r=0;;r+=1){u=n[r];if(!u)break;i=u.getAttribute("data-icon"),i&&e(u,i),o=u.className,o=o.match(/icon-[^\s'"]+/),o&&t[o[0]]&&e(u,t[o[0]])}};
